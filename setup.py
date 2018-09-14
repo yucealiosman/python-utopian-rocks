e94093a2f8e178ee7e667a855909b8df48b2ff30
Ali Osman Yüce <ali.yuce@gozen.systems>
2018-09-14 10:05:54 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='python_utopian_rocks',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/yucealiosman/python-utopian-rocks',
+    license='MIT',
+    author='Ali Osman Yuce',
+    author_email='aliosmanyuce@gmail.com',
+    description='An API wrapper for utopian.rocks',
+    install_requires=['requests'],
+    extras_require={
+        'dev': [
+            'requests_mock'
+        ]
+    },
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+    ),
+)
